db6983dc836be2b8bf1c59da8c958a8ab3f0145b
Rex Raphael <rex.raphael@outlook.com>
2026-02-25 10:02:58 -0600

    feat(id): add ID and Prefix types for CtrlPlane entities

---
+++ id.go
@@ -0,0 +1,9 @@
+package ctrlplane
+
+import "github.com/xraph/ctrlplane/id"
+
+// ID is the primary identifier type for all CtrlPlane entities.
+type ID = id.ID
+
+// Prefix identifies the entity type encoded in a TypeID.
+type Prefix = id.Prefix
